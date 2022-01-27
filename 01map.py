2a84ddf4d4fe9f1017850c04e5714e682a90cfbe
Vinay Kumar Paspula <vinaykmr149@gmail.com>
2022-01-27 17:20:25 -0600

    Initial commit

---
+++ 01map.py
@@ -0,0 +1,16 @@
+# Vinay Paspula
+# This is to practice the use of the map function
+
+
+f = open("purchases.txt","r")  # open file, read-only
+o = open("result01.txt", "w") # open file, write
+for line in f:  
+    rowList = line.strip().split("    ") 
+    print (rowList )
+    print (len(rowList ))
+    if len(rowList) == 6:
+        date, time, location, dept, amount, payType = rowList  #assign names
+        print ("{0}\t{1}".format(location, amount))
+        o.write("{0}\t{1}\n".format(location, amount))
+f.close()
+o.close()
